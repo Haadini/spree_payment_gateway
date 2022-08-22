cb8acf41c3b612b290813e94b991623d539f4c53
Hadi Heydari <haadiheydaari@gmail.com>
2022-08-22 12:04:05 +0430

    Update engine.rb

--- spree_hadi_p_g/lib/spree_hadi_p_g/engine.rb
+++ spree_hadi_p_g/lib/spree_hadi_p_g/engine.rb
@@ -10,6 +10,9 @@ module SpreeHadiPG
     config.generators do |g|
       g.test_framework :rspec
     end
+    
+    initializer 'spree.hadi_p_g.payment_methods', :after => "spree.register.payment_methods" do |app|
+      app.config.spree.payment_methods << Spree::Gateway::HadiPG
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
@@ -18,9 +21,7 @@ module SpreeHadiPG
     end
 
     config.to_prepare(&method(:activate).to_proc)
-
-    initializer 'spree.hadi_p_g.payment_methods', :after => "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::HadiPG
+    
     end  
   end
 end
