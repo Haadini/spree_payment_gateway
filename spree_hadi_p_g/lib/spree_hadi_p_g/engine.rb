ce06f83e89fd357fcb920b69bae969605e42881b
Hadi Heydari <haadiheydaari@gmail.com>
2022-08-22 11:10:26 +0430

    Update engine.rb

--- spree_hadi_p_g/lib/spree_hadi_p_g/engine.rb
+++ spree_hadi_p_g/lib/spree_hadi_p_g/engine.rb
@@ -20,7 +20,7 @@ module SpreeHadiPG
     config.to_prepare(&method(:activate).to_proc)
 
     initializer 'spree.hadi_p_g.payment_methods', :after => "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::HadiPG
+      app.config.spree.payment_methods << Spree::Gateway::HadiPG
     end  
   end
 end
