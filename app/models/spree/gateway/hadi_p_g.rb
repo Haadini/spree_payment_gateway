dcc8ae921a48cf0cabf817d52e74c26daefd2358
Hadi Heydari <haadiheydaari@gmail.com>
2022-08-22 17:04:01 +0430

    Update hadi_p_g.rb

--- app/models/spree/gateway/hadi_p_g.rb
+++ app/models/spree/gateway/hadi_p_g.rb
@@ -1,5 +1,17 @@
+require 'paypal-sdk-merchant'
 module Spree
   class Gateway::HadiPG < Gateway
-    
+    def provider_class
+      ::PayPal::SDK::Merchant::API
+    end
+
+    def provider
+      ::PayPal::SDK.configure(
+        :mode      => preferred_server.present? ? preferred_server : "sandbox",
+        :username  => preferred_login,
+        :password  => preferred_password,
+        :signature => preferred_signature)
+      provider_class.new
+    end
   end
 end
